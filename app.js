46fcf00478c074c627221718be9913bce950cfb6
Shon Harsh <shon.harsh@gmail.com>
2021-05-17 19:16:48 -0400

    Add comments and mongodb connection string

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const express = require("express");
 const mongoose = require('mongoose');
+const password = process.env.PASSWORD;
+const portListen = 3000;
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,13 +16,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/blogDB");
+mongoose.connect("mongodb+srv://admin-shon:" + password + "@cluster0.1gqte.mongodb.net/blogDB?retryWrites=true&w=majority");
+mongoose.connect(mongoDB, {
+useNewUrlParser: true,
+useUnifiedTopology: true
+});
 
 const postSchema = {
   title: String,
   content: String
 };
-
 const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function(req, res){
@@ -33,17 +39,17 @@ app.get("/", function(req, res){
   });
 });
 
+//compose get
 app.get("/compose", function(req, res){
   res.render("compose");
 });
-
+//compose post
 app.post("/compose", function(req, res){
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
   });
-
-
+  //save
   post.save(function(err){
     if (!err){
         res.redirect("/");
@@ -51,10 +57,11 @@ app.post("/compose", function(req, res){
   });
 });
 
+//get specific post
 app.get("/posts/:postId", function(req, res){
 
 const requestedPostId = req.params.postId;
-
+  //db.collection.findOne()
   Post.findOne({_id: requestedPostId}, function(err, post){
     res.render("post", {
       title: post.title,
@@ -64,15 +71,17 @@ const requestedPostId = req.params.postId;
 
 });
 
+//about
 app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
 });
 
+//contact
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+//listen
+app.listen(portListen, function() {
+  console.log("Server started on port " + portListen);
 });
